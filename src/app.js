e6a6b675aac1305015a120de8d161eda8614ef72
arunk555 <arunkurusev@gmail.com>
2023-09-07 15:05:38 +0000

    latest changes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = require('./routes');
+const errorhandler = require('./middlewares/errorhandler');
 const app = express();
 
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: false
-  })
+    express.urlencoded({
+        extended: false
+    })
 );
 
 app.use(router);
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'Welcome!'
+    });
+});
+
+app.all('*', (_, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Requested url not found!'
+    });
+});
+
+app.use(errorhandler);
 module.exports = app;
